a5fbf5ba552c7c99a546e5433eaeb8559bf80d5a
Martin Stepanek <martin.stepanek@vitality.co.uk>
2023-06-27 09:31:56 +0200

    Update to Health 7.0.0

--- android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
+++ android/src/main/kotlin/cachet/plugins/health/HealthPlugin.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Handler
+import android.os.Build
 import android.util.Log
 import androidx.annotation.NonNull
 // import androidx.compose.runtime.mutableStateOf
